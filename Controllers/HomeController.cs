70288f90a6e6d3556ad9edafdef14595b0eb930c
Jurij Befus <befus@ergonintern.de>
2018-06-11 17:03:08 +0200

    getGoal/id implemented

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,17 +9,37 @@ namespace aspnetAndReact.Controllers
     [Route("api/[controller]")]
     public class HomeController : Controller
     {
+
         [HttpGet("[action]")]
-        public JsonResult GetGoals()
+        public IActionResult GetGoals()
         {
-            return new JsonResult(GoalsMockData.Current.Goals);
+            try
+            {
+                return new JsonResult(GoalsMockData.Current.Goals);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
         }
-        
+        [HttpGet("[action]/{id}")]
+        public IActionResult GetGoal(int id)
+        {
+            try
+            {
+                return new JsonResult(GoalsMockData.Current.Goals[id]);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+
         public IActionResult Index()
         {
             try
             {
-
                 return Ok("Index home got called");
             }
             catch (Exception)
