4523ebdcc88774b2543fe31e67bba2c98fae1241
Jurij Befus <befus@ergonintern.de>
2018-11-16 16:39:41 +0100

    restructure and cleanup (not finished)

--- Models/PostDTO.cs
+++ Models/PostDTO.cs
@@ -3,19 +3,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace aspnetAndReact.Models
 {
-    public class PostDTO   
+    public class PostCreateDTO
     {
-        public int ID { get; set; }
-        public int GoaldID { get; set; }    
+
+        public int Id { get; set; }
+
+        [MaxLength(50)]
         public string Header { get; set; }
+        [Required]
+        [MaxLength(200)]
         public string Content { get; set; }
-        
-        //DRGG: To be replaced by image or reference type
+
+        //DRGG: To be replaced by binary or reference type
         public List<int?> PictureID { get; set; }
+        [Required]
         public DateTime DateCreated { get; set; }
-        
+
     }
 }
